e3725879b63c269b64f680404f9771b839e14a12
EvgeniiaBubenkova <bubenkova87@gmail.com>
2024-05-12 17:53:08 -0500

    Only what is needed

---
+++ python_beginning/6_what_needed.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "There should be one-- and preferably only one --obvious way to do it. Ithough that way may not be obvious at first unless you're Dutch"
+print(str[26:45], str[56:59], str[-19:-13])
+
+
+
